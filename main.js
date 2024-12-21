401ed364e487c8bb986d3d4ddc4dc56aa00733d3
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-12-21 15:15:28 -0300

    finished

---
+++ main.js
@@ -0,0 +1,11 @@
+const conatiner = document.getElementById('container')
+const registerBtn = document.getElementById('register')
+const loginBtn = document.getElementById('login')
+
+registerBtn.addEventListener("click", () => {
+    conatiner.classList.add('active')
+})
+
+loginBtn.addEventListener("click", () => {
+    conatiner.classList.add('active')  
+})
